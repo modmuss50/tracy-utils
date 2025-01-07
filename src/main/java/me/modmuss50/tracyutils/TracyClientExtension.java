b3f94bbe95ced7292c8355c67795054849658c45
Jochen Jacobs <JochenJacobs33@gmail.com>
2025-01-07 19:22:24 +0100

    fix isConnected

--- src/main/java/me/modmuss50/tracyutils/TracyClientExtension.java
+++ src/main/java/me/modmuss50/tracyutils/TracyClientExtension.java
@@ -3,12 +3,11 @@ package me.modmuss50.tracyutils;
 import me.modmuss50.tracyutils.mixin.TracyClientAccessor;
 
 public class TracyClientExtension {
-
     public static boolean isConnected(){
-        if (!TracyClientAccessor.isLoaded()) {
+        if (TracyClientAccessor.isLoaded()) {
             return TracyBindingsExtension.isConnected();
         } else {
             return false;
         }
-    };
+    }
 }
